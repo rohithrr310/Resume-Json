e8efa8235a35dfc328ce13217f124f273069c991
rohithrr310 <123538673+rohithrr310@users.noreply.github.com>
2023-04-20 17:05:55 +0530

    Add files via upload

---
+++ Day_3/script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "ROHITH RAJ",
+      "email": "rohithraj25111997@gmail.com",
+      "phone": 6374742727,
+      "degree": "B.SC-Computer Science",
+      "location": {
+        "address": "4/19 Pudur Ashok Nagar",
+        "postalCode": 600083,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/rohith-raj-95992a260/",
+          "github":"https://github.com/rohithrr310"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Merit Data And Technology",
+        "position": "Senior Tele Research Analyst",
+        "startDate": "2023-01-03",
+        "endDate": "9999-99-99",
+        "summary": "i am a senior research analyst handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "SRM University",
+        "department": "Computer Science",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 5.9,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        "project": [
+          "online vegetable selling platform"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "music,gaming",
+      }
+    ]
+  }
+  console.log(myResume);
